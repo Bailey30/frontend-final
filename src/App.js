c445a175fdd71327b97d8500d4b583b48e5532e8
Alex <baileymk@hotmail.com>
2022-01-05 15:03:55 +0000

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import './App.css';
 import Home from "./pages/Home"
+import Basket from "./pages/Basket"
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route to="/" element={<Home/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+        {/* <Route path="/login" element={<Login/>}></Route> */}
+        {/* <Route path="/basket" element={<Basket/>}></Route> */}
+        {/* <Route path="/about" element={<About/>}></Route> */}
+        {/* <Route path="/basket" */}
       </Routes>
 
     </Router>
