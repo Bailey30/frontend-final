4f223b71020ef6a9f8fd78ffb5675812972e79cc
Anthony White <AnthonyRichardWhite@gmail.com>
2022-01-05 16:33:12 +0000

    about page

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState} from "react"
+import styled from "styled-components"
+import Navbar from "..components"
+
+const Container = styled.div`
+margin: 0px;
+display: flex;
+position: fixed;
+
+`
+function About() {
+    return (
+        <Container>
+            <p>FINAL PROJECT</p>
+        </Container>
+    )
+}
+
+export default About
