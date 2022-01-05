295e896de0b9825da104173d5f2ac3a6fe331770
Alex <baileymk@hotmail.com>
2022-01-05 10:53:27 +0000

    added Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+
+const Container = styled.div``
+
+function Home() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
+
+export default Home
